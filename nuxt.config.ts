34a011c1c85b2068c8cfc6b5d7a60c65909db6a9
6chaoran <6chaoran@gmail.com>
2025-08-24 10:06:34 +0800

    add firebase hosting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  runtimeConfig: {
-    public:{
-      // supabaseKey: process.env.NUXT_SUPABASE_KEY,
-      // supabaseUrl: process.env.NUXT_SUPABASE_URL,
-    }
+  nitro: {
+    firebase: {
+      gen: 2,
+      httpsOptions: {
+        region: 'asia-east1',
+        maxInstances: 1,
+      },
+      nodeVersion: '18' // Can be '16' or '18' or '20'
+
+    },
   },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
